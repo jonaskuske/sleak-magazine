abf54e56c4ece72d91a0ae759d184e34b4815c84
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-11-01 23:42:46 +0100

    add team images

--- src/team.js
+++ src/team.js
@@ -4,3 +4,14 @@ import { shrug } from './utils';
 
 const { hash } = window.location;
 if (hash) shrug(hash.slice(1)); // 🤷🏻‍
+
+const options = { threshold: 0.75 };
+const handleIntersection = entries => {
+  entries.forEach(({ target, isIntersecting }) => {
+    if (isIntersecting) target.classList.add('show-image');
+  });
+};
+
+const teamObserver = new IntersectionObserver(handleIntersection, options);
+const observeTeamMember = teamObserver.observe.bind(teamObserver);
+document.querySelectorAll('.article__teaser').forEach(observeTeamMember);
