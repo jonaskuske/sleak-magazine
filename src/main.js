33fda5c2c410208816ba48e34d197fe0936896fe
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2018-11-20 21:09:17 +0100

    fix: ensure appshell is split into separate bundle

--- src/main.js
+++ src/main.js
@@ -1,37 +1,36 @@
-import './utils/appshell';
 import Stickyfill from 'stickyfilljs';
 
 import { $, debounce } from './utils';
 import { startScrollObserver, loadArticle } from './utils/load-article';
 
-// Prüfen, ob Gerät ein Touch-Interface hat
-const deviceSupportsTouch = Boolean(
-  'ontouchstart' in window ||
-    window.navigator.maxTouchPoints > 0 ||
-    window.navigator.msMaxTouchPoints > 0 ||
-    (window.DocumentTouch && document instanceof DocumentTouch),
-);
-if (deviceSupportsTouch) document.body.classList.add('supports-touch');
-
-// Mobile Viewport-Größe manuell berechnen, vgl.:
-// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-const updateWindowHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-updateWindowHeight();
-window.addEventListener('resize', debounce(updateWindowHeight, 500));
-
-// HTML Elemente
-const splash = $('.js-splash');
-const main = $('.js-main');
-
-// Bei Klick auf Splashscreen zu Content scrollen
-splash.addEventListener('click', () => {
-  main.scrollIntoView({ behavior: 'smooth', block: 'start' });
-});
-
 async function init() {
+  // Prüfen, ob Gerät ein Touch-Interface hat
+  const deviceSupportsTouch = Boolean(
+    'ontouchstart' in window ||
+      window.navigator.maxTouchPoints > 0 ||
+      window.navigator.msMaxTouchPoints > 0 ||
+      (window.DocumentTouch && document instanceof DocumentTouch),
+  );
+  if (deviceSupportsTouch) document.body.classList.add('supports-touch');
+
+  // Mobile Viewport-Größe manuell berechnen, vgl.:
+  // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+  const updateWindowHeight = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+  updateWindowHeight();
+  window.addEventListener('resize', debounce(updateWindowHeight, 500));
+
+  // HTML Elemente
+  const splash = $('.js-splash');
+  const main = $('.js-main');
+
+  // Bei Klick auf Splashscreen zu Content scrollen
+  splash.addEventListener('click', () => {
+    main.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+
   const { hash } = window.location;
   const targetId = hash.slice(1);
   const targetArticle = await loadArticle(targetId);
@@ -61,4 +60,4 @@ async function init() {
 }
 
 // Go!
-init();
+setTimeout(init, 0);
--- src/team.js
+++ src/team.js
@@ -1,16 +1,21 @@
-import './utils/appshell';
 import { updateHash } from './utils';
 
-const options = { rootMargin: `-${Math.floor(window.innerHeight / 2)}px 0px` };
-const handleIntersection = entries => {
-  entries.forEach(({ target, isIntersecting }) => {
-    if (isIntersecting) {
-      updateHash(target.id);
-      target.classList.add('show-image');
-    }
-  });
+const init = () => {
+  const options = {
+    rootMargin: `-${Math.floor(window.innerHeight / 2)}px 0px`,
+  };
+  const handleIntersection = entries => {
+    entries.forEach(({ target, isIntersecting }) => {
+      if (isIntersecting) {
+        updateHash(target.id);
+        target.classList.add('show-image');
+      }
+    });
+  };
+
+  const teamObserver = new IntersectionObserver(handleIntersection, options);
+  const observeTeamMember = teamObserver.observe.bind(teamObserver);
+  document.querySelectorAll('article').forEach(observeTeamMember);
 };
 
-const teamObserver = new IntersectionObserver(handleIntersection, options);
-const observeTeamMember = teamObserver.observe.bind(teamObserver);
-document.querySelectorAll('article').forEach(observeTeamMember);
+setTimeout(init, 0);
